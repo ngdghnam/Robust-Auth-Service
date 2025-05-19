7890b65cad566038a1d2bd88ac72ed0c28f8df8e
ngdghnam <stghoainam4002@gmail.com>
2025-05-19 09:42:33 +0700

    Define custom code and status

---
+++ src/enums/status.enum.ts
@@ -0,0 +1,9 @@
+export enum Status {
+  SUCCESS = "success",
+  PATH_NOT_FOUND = "path not found",
+  BAD_REQUEST = "bad request",
+  CREATED = "Created",
+  INTERNAL_SERVER_ERROR = "Internal server error",
+  UNAUTHORIZED = "Unauthorized",
+  FORBIDDEN = "Forbidden",
+}
