5a1400162badc4f45587c7e6b9b989a7516bbe6c
ngdghnam <stghoainam4002@gmail.com>
2025-05-23 01:09:39 +0700

    Auto generate random password

---
+++ src/utils/passwordGen.ts
@@ -0,0 +1,15 @@
+export function generateRandomPassword(length: number = 10) {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
+  let password = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    password += characters.charAt(randomIndex);
+  }
+  return password;
+}
+
+// const passwordLength = 12;
+const newPassword = generateRandomPassword();
+console.log(">>>", newPassword);
